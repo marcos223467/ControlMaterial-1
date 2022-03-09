deff4d1cb12a9ee222c1012c2b4e3902a9b682f1
jaume89 <jaumeborja89@gmail.com>
2022-03-09 10:18:25 +0100

    base de datos

--- materiales.sql
+++ materiales.sql
@@ -12,11 +12,23 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla material.material: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+REPLACE INTO `material` (`id`, `descripcion`, `cantidad`, `categoria`) VALUES
+	(1, 'tablet', 20, 'tablet'),
+	(2, 'portatil', 30, 'portatil');
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla material.reservas: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `reservas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservas` ENABLE KEYS */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla material.usuarios: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+REPLACE INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `rol`) VALUES
+	(1, 'Marcos', 'Cantos Niñerola', 'marcos@gmail.com', 'material', 'admin'),
+	(3, 'Jaume', 'Borja', 'jaume@gmail.com', '1234', 'profe');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
