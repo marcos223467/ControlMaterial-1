1efb31752fc2730bbb04bc52b18a5837095e37b5
jaume89 <jaumeborja89@gmail.com>
2022-03-10 14:01:26 +0100

    cambios reservar

---
+++ controller/buscarid.php
@@ -0,0 +1,21 @@
+<?php
+
+    require '../database.php';
+    session_start();
+
+    if ($_POST['id'] != "") {
+        $id = $_POST['id'];
+
+        $ins = "SELECT cantidad FROM material WHERE id = '{$id}'";
+        $query = $conn->query($ins);
+        $count = $query->rowCount();
+        if ($count != 0) {
+            foreach ($query as $row) {
+                $cantstr = $row['cantidad'];
+                $cant = intval($cantstr);
+                echo json_encode($cant);
+            }
+        }
+    }
+
+?>
--- reservar.php
+++ reservar.php
@@ -18,6 +18,8 @@
             header("location: index.php");
         }
 
+        $select = "SELECT id, categoria FROM material";
+        $query = $conn->query($select);
 
 ?>
 
@@ -41,6 +43,7 @@
         <script>
             $(document).ready(function(){
                 $("#cant").hide();
+                $("#mas").hide();
                 var hojasDeEstilo = document.styleSheets;
                 var ultimaHoja = hojasDeEstilo[hojasDeEstilo.length-1];
                 ultimaHoja.insertRule("#mat{ width:18rem; }");
@@ -51,14 +54,52 @@
 
                 if ($('#mat').val() != "0") {
                     $("#cant").show();
+                    $("#mas").show();
                     material.style.setProperty("width", "14rem");
                     cantidad.style.setProperty("width", "4rem");
                     var valMaterial = $("#mat").val();
-                    $("#id_material").val(valMaterial);
+                    console.log(valMaterial);
+
+                    $.ajax({
+                        url:"controller/buscarid.php",
+                        type:"POST",
+                        data:{
+                            id: valMaterial,
+                        },
+                        success: function(cant){
+                            console.log(cant);
+                            var select = document.getElementById("cant");
+                            while (select.firstChild) {
+                                select.removeChild(select.firstChild);
+                            }
+                            for (let index = 1; index <= cant; index++) {
+                                var option = document.createElement("option");
+                                option.value = index;
+                                option.innerHTML = index;
+                                select.appendChild(option);
+                            }
+                        },
+                    });
                 } else {
                     $("#cant").hide();
+                    $("#mas").hide();
                     material.style.setProperty("width", "18rem");
                 }
+            };
+            function otro(){
+                var select = document.createElement("select");
+                selects.appendChild(select);
+                <?php
+                    $select1 = "SELECT id, categoria FROM material";
+                    $query1 = $conn->query($select1);
+                ?>
+                id.forEach(<?php $query1 ?> => row{
+                    console.log(row);
+                });
+                var option = document.createElement("option");
+                option.value = options;
+                option.innerHTML = options;
+                select.appendChild(option);
             };
         </script>
 
@@ -80,7 +121,7 @@
             <p class="card-text">Hora de la devolución:</p>
             <p class="card-text"><input type="time" name="hdevolucion" id="hdevolucion"></p>
             <p class="card-text">Elije material y cantidad:</p>
-            <p class="card-text">
+            <p class="card-text" id="selects">
                 <select onchange="cambio()" name="material" id="mat">
                     <option value="0">--Elije el material--</option>
                     <?php
@@ -91,13 +132,10 @@
                         }
                     ?>
                 </select>
-                <input type="hidden" value="" id="id_material">
                 <select name="cantidad" id="cant">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
                 </select>
             </p>
+            <button type="button" class="btn btn-success mb-3" id="mas" onclick="otro()"><i class="fas fa-thin fa-plus"></i></button><br>
             <button class="btn btn-dark" type="button" id="btn">Reservar</button>
         </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
