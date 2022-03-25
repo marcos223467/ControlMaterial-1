46b955fdff3fa4cb9e4f7fa0d304398a163bfc54
jaume89 <jaumeborja89@gmail.com>
2022-03-25 13:38:46 +0100

    buscarMat arreglado

--- controller/buscarMat.php
+++ controller/buscarMat.php
@@ -8,25 +8,16 @@
         $horaInicio = $_POST['horaInicio'];
         $horaFin = $_POST['horaFin'];
     }
-
-    $select = "SELECT cantidad_y_material FROM reservas WHERE fecha_inicio = '$fecha'";
+    $select = "SELECT id, descripcion FROM material ORDER BY categoria";
     $query = $conn->query($select);
     $count = $query->rowCount();
-
-    if ($count == 0) {
-        $select1 = "SELECT id, descripcion FROM material ORDER BY categoria";
-        $query1 = $conn->query($select1);
-        $count1 = $query1->rowCount();
-        if ($count1 != 0) {
-            foreach ($query1 as $row1) {
-                $id = intval($row1['id']);
-                $descripcion = $row1['descripcion'];
-                $envio = array($id, $descripcion);
-                echo json_encode($envio);
-            }
+    if ($count != 0) {
+        foreach ($query as $row) {
+            $id = intval($row['id']);
+            $descripcion = $row['descripcion'];
+            $envio = array($id, $descripcion);
+            echo json_encode($envio);
         }
-    } else {
-
     }
 
 ?>
